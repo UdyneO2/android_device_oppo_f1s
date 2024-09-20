472d7c6893d8e56dce85e4d135d22a00d2ecc99f
UdyneO2 <dynemuhamad@gmail.com>
2024-09-20 13:42:14 +0700

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -2,13 +2,13 @@
 #$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := M3s
+PRODUCT_RELEASE_NAME := f1s
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/meizu/m3s/device_m3s.mk)
+$(call inherit-product, device/oppo/f1s/device_f1s.mk)
 
 # Configure dalvik heap
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
@@ -20,8 +20,8 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := m3s
-PRODUCT_NAME := cm_m3s
-PRODUCT_BRAND := Meizu
-PRODUCT_MODEL := M3s
-PRODUCT_MANUFACTURER := Meizu
+PRODUCT_DEVICE := f1s
+PRODUCT_NAME := cm_f1s
+PRODUCT_BRAND := Oppo
+PRODUCT_MODEL := F1s
+PRODUCT_MANUFACTURER := Oppo

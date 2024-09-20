dab1f056e8d7be19ddfd9dd2ee7d6b5117535ada
UdyneO2 <dynemuhamad@gmail.com>
2024-09-20 13:43:16 +0700

    Update device_f1s.mk

--- device_m3s.mk
+++ device_m3s.mk
@@ -1,11 +1,11 @@
 $(call inherit-product, device/meizu/m3_m3s-common/device-common.mk)
 $(call inherit-product-if-exists, vendor/meizu/m3s/m3s-vendor.mk)
 
-LOCAL_PATH := device/meizu/m3s
+LOCAL_PATH := device/oppo/f1s
 
-TARGET_OTA_ASSERT_DEVICE := m3s
+TARGET_OTA_ASSERT_DEVICE := f1s
 LOCAL_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
 
 # BT A2DP
 PRODUCT_COPY_FILES += \
-    vendor/meizu/m3s/proprietary/vendor/lib/hw/audio.a2dp.blueangel.so:system/vendor/lib/hw/audio.a2dp.mt6750.so
+    vendor/oppo/f1s/proprietary/vendor/lib/hw/audio.a2dp.blueangel.so:system/vendor/lib/hw/audio.a2dp.mt6750.so
